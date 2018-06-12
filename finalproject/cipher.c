81271c14cd1df7036af6dfc150bf70fe40029a1f
anikamalladi <33032951+anikamalladi@users.noreply.github.com>
2018-06-12 12:39:03 -0700

    Update cipher.c

--- finalproject/cipher.c
+++ finalproject/cipher.c
@@ -1,1 +1,27 @@
+/*
+   cipher.c
 
+   Sample program for COMP1917 Lab 7
+*/
+
+#include <stdio.h>
+
+#include "vigenere.h"
+
+char key[MAX_KEY];
+
+int main( void )
+{
+  int ch;
+
+  printf("Enter key: ");
+  scanf( "%s",  key );
+
+  printf("Enter text:\n");
+  while(( ch = getchar()) != EOF ) {
+     encrypt( ch );
+  }
+  printf( "\n" );
+
+  return 0;
+}
